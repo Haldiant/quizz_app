bae69b1321e556585abaa84b2262ff9ddfa49c5f
Haldiant <nhalwidinato@gmail.com>
2025-05-13 19:59:43 +0700

    Add QuestionsScreen implementation and AnswerButton widget; update README with usage notes

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:quizz_app/answer_button.dart';
+import 'package:quizz_app/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -10,6 +12,25 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return Text('Questions Screen');
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            currentQuestion.questions,
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(height: 40),
+
+          AnswerButton(answerText: currentQuestion.answer[0], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answer[1], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answer[2], onTap: () {}),
+          AnswerButton(answerText: currentQuestion.answer[3], onTap: () {}),
+        ],
+      ),
+    );
   }
 }
